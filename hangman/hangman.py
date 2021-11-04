d560e180b0bd18c3d0475c37f528cd661004f2b5
Georgi Dafov <gs.dafov@gmail.com>
2021-11-04 04:05:52 +0200

    Words cleaned

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -5,7 +5,7 @@ import string
 
 def get_valid_word(words):
     word = random.choice(words)
-    while '-' in word or ' ' in word:
+    while '-' in word:
         word = random.choice(words)
     return word.upper()
 
