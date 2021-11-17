4cec3621effe3a24f677b73fc009680292640b32
Georgi Dafov <gs.dafov@gmail.com>
2021-11-17 20:17:51 +0200

    TicTacToe

--- tic-tac-toe/game.py
+++ tic-tac-toe/game.py
@@ -10,6 +10,8 @@ class TicTacToe:
     @staticmethod
     def print_board_nums():
         # 0 | 1 | 2
+        # 3 | 4 | 5
+        # 6 | 7 | 8
         number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
         for row in number_board:
             print('| ' + ' | '.join(row) + ' |')       
